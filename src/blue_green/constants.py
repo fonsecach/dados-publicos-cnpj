3be21e64700c65e141122e1b0b6f7f336cf61d77
Cleiton <chdf@proton.me>
2026-05-31 10:06:41 -0300

    feat(blue-green): add blue_green package with table/index constants

---
+++ src/blue_green/constants.py
@@ -0,0 +1,22 @@
+EXPECTED_TABLES = [
+    "empresa",
+    "estabelecimento",
+    "socios",
+    "simples",
+    "cnae",
+    "motivo",
+    "municipio",
+    "natureza",
+    "pais",
+    "qualificacao",
+]
+
+EXPECTED_INDEXES = [
+    "empresa_cnpj",
+    "estabelecimento_cnpj",
+    "estabelecimento_cnpj_completo",
+    "socios_cnpj",
+    "simples_cnpj",
+    "estabelecimento_situacao",
+    "estabelecimento_municipio",
+]
